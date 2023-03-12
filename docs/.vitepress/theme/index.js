afea70bd57306681bbb0525f09d871856f7c3b0c
ChenYuTong10 <894104315@qq.com>
2023-03-12 21:26:43 +0800

    Custom theme style

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,3 +1,7 @@
+// more to see official documentation https://vitepress.dev/guide/extending-default-theme
+// Notice custom css style should be imported after the default theme.
+import DefaultTheme from 'vitepress/theme'
+import "./style/custom.css";
 import "./style/mathjax3.css";
 
-export { default } from 'vitepress/theme';
+export default DefaultTheme;
